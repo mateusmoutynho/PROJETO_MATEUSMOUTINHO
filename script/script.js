ebe43f50c674536fae803617bfb105386f2d8aa5
mateusmoutynho <mateusmoutinho79@gmail.com>
2021-04-16 17:48:19 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,6 @@
+let hamburguinho = document.querySelector('.hamburguinho')
+let nav = document.querySelector('.header nav')
+
+hamburguinho.addEventListener('click', function() {
+  nav.classList.toggle('nav2')
+});
